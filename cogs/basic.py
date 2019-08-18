ff95ce8c716207b4bbc5712d1ac85d0beb85bb24
Some Guy <ozruen@gmail.com>
2019-08-19 01:24:14 +0800

    Re-modified help command

--- cogs/basic.py
+++ cogs/basic.py
@@ -39,13 +39,13 @@ class Basic(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Define a new command
     @commands.command(
-        name='help',
-        description='The help command!',
-        aliases=['commands', 'command'],
-        usage='cog'
+        name = 'help',
+        description = "The help command!",
+        aliases = ['commands', 'command']
     )
-    async def help_command(self, ctx, cog='all'):
+    async def help_command(self, ctx, command='all'):
         # The third parameter comes into play when only one word argument has to be passed by the user
         # Prepare the embed
         help_embed = discord.Embed(
@@ -57,65 +57,45 @@ class Basic(commands.Cog):
         cogs = [c for c in self.bot.cogs.keys()]
 
         # If cog is not specified by the user, we list all cogs and commands
-        if cog == 'all':
+        if command == 'all':
             for cog in cogs:
                 # Get a list of all commands under each cog
                 cog_commands = self.bot.get_cog(cog).get_commands()
-                commands_list = ''
+                commands_list = ""
                 for comm in cog_commands:
-                    commands_list += f'**{comm.name}** - *{comm.description}*\n'
+                    commands_list += f"{comm.name}\n"
 
-                # Add the cog's details to the embed.
                 help_embed.add_field(
                     name = cog,
                     value = commands_list,
                     inline = False
                 )
+
+            await ctx.send(embed = help_embed)
+            return
         else:
-            # If the cog was specified
-            lower_cogs = [c.lower() for c in cogs]
+            for cog in cogs:
+                cog_commands = self.bot.get_cog(cog).get_commands()
+                for comm in cog_commands:
+                    if command == comm.name:
+                        help_text = f"**{comm.name}**\n\n"
+                        help_text += comm.description
 
-            # If the cog actually exists.
-            if cog.lower() in lower_cogs:
-                # Get a list of all commands in the specified cog
-                commands_list = self.bot.get_cog(cogs[lower_cogs.index(cog.lower())]).get_commands()
-                help_text=''
-                # Add details of each command to the help text
-                # Command Name
-                # Description
-                # [Aliases]
-                #
-                # Format
-                for command in commands_list:
-                    help_text += f'```{command.name}```\n' \
-                        f'**{command.description}**\n\n'
+                        if len(comm.aliases) > 0:
+                            help_text += f"\n\n**Aliases :** `{'`, `'.join(comm.aliases)}`"
 
-                    # Also add aliases, if there are any
-                    if len(command.aliases) > 0:
-                        help_text += f'**Aliases :** `{"`, `".join(command.aliases)}`\n\n\n'
-                    else:
-                        # Add a newline character to keep it pretty
-                        # That IS the whole purpose of custom help
-                        help_text += '\n'
+                        help_embed.description = help_text
+                        await ctx.send(embed = help_embed)
+                        return
 
-                    # Finally the format
-                    help_text += f'Format: `@{self.bot.user.name}#{self.bot.user.discriminator}' \
-                        f' {command.name} {command.usage if command.usage is not None else ""}`\n\n\n\n'
-
-                help_embed.description = help_text
-            else:
-                # Notify the user of invalid cog and finish the command
-                await ctx.send('Invalid cog specified.\nUse `help` command to list all cogs.')
-                return
-
-        await ctx.send(embed = help_embed)
+        # Notify the user of invalid command and finish the command
+        await ctx.send('Invalid command specified.\nUse `help` command to list all command.')
         return
 
-    # Define a new command
     @commands.command(
-        name="ping",
-        description="To check if I'm alive.",
-        aliases=["p"]
+        name = 'ping',
+        description = "To check if I'm alive.",
+        aliases = ['p']
     )
     async def ping_command(self, ctx):
         start = datetime.timestamp(datetime.now())

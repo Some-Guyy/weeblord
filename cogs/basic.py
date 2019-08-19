b6df3eca5a28608e7c395e6e16fdf6b3a25bdba3
Some Guy <ozruen@gmail.com>
2019-08-19 16:26:27 +0800

    Updated version number

--- cogs/basic.py
+++ cogs/basic.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from datetime import datetime
 
 # Bot version
-version = "0.1.0"
+version = "0.1.2"
 
 # These color constants are taken from discord.js library
 colors = {

836f0b04b62b68ed23efa4a23b7648f2e6c967a8
Some Guy <ozruen@gmail.com>
2019-08-23 10:14:35 +0800

    0.5.5 - Cleaned up code for main.py and basic cog

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ f = open('../token/weeblord', 'rb')
 token = pickle.load(f)
 f.close()
 
-info = '[INFO]'
-
 bot = commands.Bot(
     command_prefix = '$',
     description = 'The lord of weebs.',
@@ -20,8 +18,8 @@ cogs = ['cogs.basic', 'cogs.fun', 'cogs.games']
 
 @bot.event
 async def on_ready():
-    print(f"{info} Weeblord has risen!")
-    print(f"{info} {datetime.now()}")
+    print(f"[INFO] Weeblord has risen!")
+    print(f"[INFO] {datetime.now()}")
     # Remove the help command before loading the cogs
     bot.remove_command('help')
     for cog in cogs:
@@ -30,3 +28,35 @@ async def on_ready():
     return
 
 bot.run(token, bot = True, reconnect = True)
+
+# # These color constants are taken from discord.js library
+# colors = {
+#   'DEFAULT': 0x000000,
+#   'WHITE': 0xFFFFFF,
+#   'AQUA': 0x1ABC9C,
+#   'GREEN': 0x2ECC71,
+#   'BLUE': 0x3498DB,
+#   'PURPLE': 0x9B59B6,
+#   'LUMINOUS_VIVID_PINK': 0xE91E63,
+#   'GOLD': 0xF1C40F,
+#   'ORANGE': 0xE67E22,
+#   'RED': 0xE74C3C,
+#   'GREY': 0x95A5A6,
+#   'NAVY': 0x34495E,
+#   'DARK_AQUA': 0x11806A,
+#   'DARK_GREEN': 0x1F8B4C,
+#   'DARK_BLUE': 0x206694,
+#   'DARK_PURPLE': 0x71368A,
+#   'DARK_VIVID_PINK': 0xAD1457,
+#   'DARK_GOLD': 0xC27C0E,
+#   'DARK_ORANGE': 0xA84300,
+#   'DARK_RED': 0x992D22,
+#   'DARK_GREY': 0x979C9F,
+#   'DARKER_GREY': 0x7F8C8D,
+#   'LIGHT_GREY': 0xBCC0C0,
+#   'DARK_NAVY': 0x2C3E50,
+#   'BLURPLE': 0x7289DA,
+#   'GREYPLE': 0x99AAB5,
+#   'DARK_BUT_NOT_BLACK': 0x2C2F33,
+#   'NOT_QUITE_BLACK': 0x23272A
+# }

206952a124a5b57fbbaedab9d546030114659496
Some Guy <ozruen@gmail.com>
2019-08-30 18:21:56 +0800

    Tsrl

--- cogs/fun.py
+++ cogs/fun.py
@@ -6,7 +6,7 @@ import random
 from nltk.corpus import wordnet
 from nltk.tokenize import word_tokenize
 from nltk.tokenize.treebank import TreebankWordDetokenizer
-from time import time
+import asyncio
 
 class Fun(commands.Cog):
     def __init__(self, bot):

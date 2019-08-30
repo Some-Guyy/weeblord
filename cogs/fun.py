8d0fe8191b30e4a477c3b1ea7a7868f20dbc56f0
Some Guy <ozruen@gmail.com>
2019-08-30 18:17:37 +0800

    Tsrl

--- cogs/fun.py
+++ cogs/fun.py
@@ -109,6 +109,60 @@ class Fun(commands.Cog):
         )
         await ctx.send(embed = tsr_embed)
 
+    @commands.command(
+        name = 'thesaurize-loop',
+        description = "Replace every word from the previous message into another word with a similar meaning. *(hopefully)*",
+        aliases = ['tsrl']
+    )
+    @commands.guild_only()
+    async def thesaurize_command(self, ctx):
+        await ctx.channel.trigger_typing()
+
+        def thesaurize(input_string):
+            skip_words = ['who']
+            tokenized_list = word_tokenize(input_string.lower())
+            thesaurized_list = []
+            for word in tokenized_list:
+                synset_list = wordnet.synsets(word)
+                thesaurized_word = word
+                try_count = 0
+                while thesaurized_word.lower() == word.lower():
+                    try_count += 1
+                    if try_count == 21:
+                        break
+                    if word.lower() in skip_words:
+                        break
+                    if len(word) < 3:
+                        break
+                    if len(synset_list) > 0:
+                        usable_synset_list = []
+                        for synset in synset_list:
+                            if len(synset.lemmas()) > 1:
+                                usable_synset_list.append(synset)
+                        if len(usable_synset_list) > 0:
+                            thesaurized_word = random.choice(random.choice(usable_synset_list).lemmas()).name()
+                        else:
+                            break
+                    else:
+                        break
+                thesaurized_list.append(thesaurized_word)
+            return TreebankWordDetokenizer().detokenize(thesaurized_list)
+
+        messages = await ctx.channel.history(limit = 2).flatten()
+        message = messages[1]
+        text = message.content
+
+        for i in range(5):
+            tsr_embed = discord.Embed(
+                description = thesaurize(text),
+                color = 0x11806A # DARK_AQUA
+            )
+            tsr_embed.set_author(
+                name = message.author.display_name,
+                icon_url = message.author.avatar_url
+            )
+            await ctx.send(embed = tsr_embed)
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))

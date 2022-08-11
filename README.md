5a881227c1f162dea32e704dce6d0efc6816f0bc
some guy <ozruen@gmail.com>
2022-08-11 22:53:47 +0800

    Re-added old files

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # Weeblord
 <img src="https://cdn.discordapp.com/attachments/625670917263196174/625680239833382913/wow.jpg" width="100" title="nice face ( ͡° ͜ʖ ͡°)" />
 
-Under construction...
+A simple discord bot with the usual normal commands. Being a weeblord, the main goal of developing this bot was to include anime-related commands. Therefore, the next step planned is to consume [MAL](https://myanimelist.net/)'s public [API](https://myanimelist.net/forum/?topicid=1804432) when its development is completed and make some simple games with it. [Invite him to your server!](https://discordapp.com/api/oauth2/authorize?client_id=614361547162255381&permissions=0&scope=bot)
+
+*Note that this bot requires NLTK's Treebank, Punkt, and Wordnet packages. Download them via:* ```nltk.download("<package name>")```
+
+### Commands (as of v1.1.24)
+**Basic**
+- help
+- feedback
+- ping
+- roll
+
+**Fun**
+- uwu-ify
+- thesaurize
+
+**Games**
+- rps
+- charge
+- whatmovie
---
+++ cogs/fun.py
@@ -0,0 +1,131 @@
+import discord
+from discord.ext import commands
+import sys
+
+import random
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+import asyncio
+
+class Fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command(
+        name = 'uwu-ify',
+        description = "Uwu-ify de pwevious message. UwU",
+        aliases = ['uwu']
+    )
+    @commands.guild_only()
+    async def uwu_command(self, ctx):
+        await ctx.channel.trigger_typing()
+
+        def uwu(string):
+            string = string.replace('l', 'w')
+            string = string.replace('L', 'W')
+            string = string.replace('r', 'w')
+            string = string.replace('R', 'W')
+            string = string.replace('thr', 'fw')
+            string = string.replace('Thr', 'Fw')
+            string = string.replace('THr', 'Fw')
+            string = string.replace('THR', 'FW')
+            string = string.replace('tHR', 'fW')
+            string = string.replace('thR', 'fW')
+            string = string.replace('th', 'd')
+            string = string.replace('Th', 'D')
+            string = string.replace('tH', 'D')
+            string = string.replace('TH', 'D')
+            string = string.replace('ou', 'uw')
+            string = string.replace('Ou', 'Uw')
+            string = string.replace('oU', 'uW')
+            string = string.replace('OU', 'UW')
+            string += " uwu"
+            return string
+
+        messages = await ctx.channel.history(limit = 2).flatten()
+        message = messages[1]
+        text = message.content
+
+        uwu_embed = discord.Embed(
+            description = uwu(text),
+            color = 0xE91E63 # LUMINOUS_VIVID_PINK
+        )
+        uwu_embed.set_author(
+            name = message.author.display_name,
+            icon_url = message.author.avatar_url
+        )
+        await ctx.send(embed = uwu_embed)
+
+    @commands.command(
+        name = 'thesaurize',
+        description = "Replace every word from the previous message into another word with a similar meaning. *(hopefully)*\nYou can also add a number `(2-5)` after the command to re-thesaurize the output that many times!",
+        aliases = ['tsr']
+    )
+    @commands.guild_only()
+    async def thesaurize_command(self, ctx, times = 1):
+        await ctx.channel.trigger_typing()
+
+        def thesaurize(input_string):
+            skip_words = ['who']
+            tokenized_list = word_tokenize(input_string.lower())
+            thesaurized_list = []
+            for word in tokenized_list:
+                synset_list = wordnet.synsets(word)
+                thesaurized_word = word
+                try_count = 0
+                while thesaurized_word.lower() == word.lower():
+                    try_count += 1
+                    if try_count == 21:
+                        break
+                    if word.lower() in skip_words:
+                        break
+                    if len(word) < 3:
+                        break
+                    if len(synset_list) > 0:
+                        usable_synset_list = []
+                        for synset in synset_list:
+                            if len(synset.lemmas()) > 1:
+                                usable_synset_list.append(synset)
+                        if len(usable_synset_list) > 0:
+                            thesaurized_word = random.choice(random.choice(usable_synset_list).lemmas()).name()
+                        else:
+                            break
+                    else:
+                        break
+                thesaurized_list.append(thesaurized_word)
+            return TreebankWordDetokenizer().detokenize(thesaurized_list)
+        
+        if 1 <= times <= 5:
+            messages = await ctx.channel.history(limit = 2).flatten()
+            message = messages[1]
+            text = message.content
+
+            for i in range(times):
+                text = thesaurize(text)
+                tsr_embed = discord.Embed(
+                    description = text,
+                    color = 0x11806A # DARK_AQUA
+                )
+                tsr_embed.set_author(
+                    name = message.author.display_name,
+                    icon_url = message.author.avatar_url
+                )
+                await ctx.send(embed = tsr_embed)
+                await asyncio.sleep(0.5)
+
+        else:
+            await asyncio.sleep(3)
+            tsr_embed = discord.Embed(
+                description = f"I am stupid enough to think that {self.bot.user.display_name} would do this {times} times.",
+                color = 0x11806A # DARK_AQUA
+            )
+            tsr_embed.set_author(
+                name = ctx.message.author.display_name,
+                icon_url = ctx.message.author.avatar_url
+            )
+            await ctx.send(embed = tsr_embed)
+
+
+def setup(bot):
+    bot.add_cog(Fun(bot))

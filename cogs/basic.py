fa4a8f2f413a9163db1e0c19bf9c319d5cf90794
some guy <ozruen@gmail.com>
2022-10-17 11:25:58 +0800

    v2.0.8 - Modified WhatMovie

--- cogs/basic.py
+++ cogs/basic.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(filename = 'logs/weeblord.log', encoding = 'utf-8', format = '%(asctime)s - %(levelname)s - %(message)s', level = logging.DEBUG)
 
 # Bot version
-version = "2.0.7"
+version = "2.0.8"
 
 # New - The Cog class must extend the commands.Cog class
 class Basic(discord.Cog):
